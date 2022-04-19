d87e5c2386a5678e4bfc4ff512e48bbab5a4417a
James Bennett <JamesBennett25565@gmail.com>
2022-04-19 15:29:41 +0100

    Anoter rewrite

--- README.md
+++ README.md
@@ -1,79 +1,35 @@
-Get a notification when certain youtube channels upload a new video!
+Get notified when Youtube videos are uploaded!
 ```js
 /*
- * Example One
+ * Example
  */
-const ytNotifs = require("youtube-notifs");
-ytNotifs.start(60, "./youtubeNotifsData.json"); // args: new video check interval in seconds, data file path
-ytNotifs.events.on("newVid", (obj) => { // obj is an object containing video info
-    console.log(ytNotifs.msg("{channelName} just uploaded a new video!\n{vidUrl}", obj));
-    /*
-     * all placeholders that can be used with ytNotifs.msg()
-     *   {vidName}
-     *   {vidUrl}
-     *   {vidDescription}
-     *   {vidId}
-     *   {vidWidth}
-     *   {vidHeight}
-     *   {vidThumbnailUrl}
-     *   {vidThumbnailWidth}
-     *   {vidThumbnailHeight}
-     *   {channelName}
-     *   {channelUrl}
-     *   {channelId}
-     */
+
+import ytNotifs from "youtube-notifs";
+
+// Args: data file name, new video check interval (seconds)
+const notifier = new ytNotifs.Notifier("./yt-notifs-data.json", 120);
+
+notifier.on("error", (err) => {
+	console.error(err);
 });
-ytNotifs.subscribe(["UC7bD_GEqdgUDCe_cyVa6Y2g"]); // args: array of channel IDs
-```
-```js
-/*
- * Example Two
- */
-const ytNotifs = require("youtube-notifs");
-ytNotifs.start(); // defaults will be used: 120, "./ytNotifsData.json"
-ytNotifs.events.on("newVid", (obj) => {
-    console.log(obj.channel.name + " just uploaded a new video!\n" + obj.vid.url);
+
+notifier.on("newVid", (obj) => {
+	console.log(
+		ytNotifs.msg("{channelName} uploaded a new video!\n{vidUrl}", obj)
+	);
 });
-ytNotifs.subscribe(["UC7bD_GEqdgUDCe_cyVa6Y2g", "UCS0N5baNlQWJCUrhCEo8WlA"]); // multiple channels can be subscribed to
-```
-```js
+
 /*
- * Example Three (uses https://www.npmjs.com/package/discord.js)
+ * Channel ID or array of channel IDs to subscribe to,
+ * this function can be ran at any time. There is also an "unsubscribe" function
  */
-const ytNotifs = require("youtube-notifs");
-ytNotifs.start();
-ytNotifs.events.on("newVid", (obj) => {
-    let discordChannelId;
-    switch (obj.vid.id) {
-        case "UC7bD_GEqdgUDCe_cyVa6Y2g":
-            discordChannelId = "521539885237272586";
-            break;
-        case "UCS0N5baNlQWJCUrhCEo8WlA":
-            discordChannelId = "804050388680310784";
-            break;
-    };
-    client.channels.get(discordChannelId).send(ytNotifs.msg("{channelName} just uploaded a new video!\n{vidUrl}", obj));
-});
-ytNotifs.subscribe(["UC7bD_GEqdgUDCe_cyVa6Y2g"]);
-ytNotifs.subscribe(["UCS0N5baNlQWJCUrhCEo8WlA"]); // you don't need to subscribe to all channels at the same time
-```
-```js
+notifier.subscribe("UC9-y-6csu5WGm29I7JiwpnA");
+
 /*
- * Examples of Other functions
+ * Start checking for uploads.
+ * There is also a "stop" function
  */
-console.log("Current subscriptions: " + ytNotifs.getSubscriptions().join(", ")); // returns an array of channels which are subscribed to
-ytNotifs.unsubscribe(["UC7bD_GEqdgUDCe_cyVa6Y2g"]); // this functions lets you unsubscribe from an array of channels
-console.log("Name of channel: " + ytNotifs.getChannelName("UC7bD_GEqdgUDCe_cyVa6Y2g")); // get the name of a channel from its ID
-ytNotifs.permanentSubscribe(["UC7bD_GEqdgUDCe_cyVa6Y2g"]); // permanently subscribe to an array of channels
-ytNotifs.permanentUnsubscribe(["UC7bD_GEqdgUDCe_cyVa6Y2g"]); // permanently unsubscribe from an array of channels
-ytNotifs.delChannelsData(["UCBa659QWEk1AI4Tg--mrJ2A"]); // permanently unsubscribe from an array of channels and delete all data about those channels
-ytNotifs.stop(); // stop looking for new videos
+notifier.start();
 ```
-```js
-/*
- * How to disable ANSI colours
- */
-const jbLogger = require("@james-bennett-295/logger");
-jbLogger.config({ useAnsiColours: false });
-```
+
 Have any issues, questions or suggestions? [Join my Discord server](https://discord.com/invite/dcAwVFj2Pf) or [open a Github issue](https://github.com/James-Bennett-295/npm-youtube-notifs/issues/new).
---
+++ index.mjs
@@ -0,0 +1,296 @@
+import EventEmitter from "node:events";
+import fs from "node:fs";
+import https from "node:https";
+import { parseString as parseXml } from "xml2js";
+import writeFile from "@james-bennett-295/writefile";
+
+const dataFileNamePattern = /^(([a-zA-Z0-9\-]*|\.{0,2})\/)*[a-zA-Z0-9\-]*\.json$/;
+const channelIdPattern = /^[a-zA-Z0-9_\-]{24}$/;
+
+class Notifier extends EventEmitter {
+
+	constructor(dataFileName, newVidCheckInterval) {
+		super();
+
+		this.valid = false;
+		this.enabled = false;
+		this.ready = false;
+		this.subscriptions = [];
+
+		if (typeof dataFileName === "undefined") {
+			dataFileName = "./yt-notifs-data.json"
+		} else if (!(
+			typeof dataFileName === "string" &&
+			dataFileNamePattern.test(dataFileName)
+		)) {
+			const error = new Error("Invalid dataFileName provided");
+			this.emit("error", error);
+			return;
+		}
+		if (typeof newVidCheckInterval === "undefined") {
+			newVidCheckInterval = 120;
+		} else if (!(
+			typeof newVidCheckInterval === "number" &&
+			newVidCheckInterval >= 15
+		)) {
+			const error = new Error("Invalid newVidCheckInterval provided");
+			this.emit("error", error);
+			return;
+		}
+		this.valid = true;
+
+		this.dataFileName = dataFileName;
+		this.newVidCheckInterval = newVidCheckInterval * 1000;
+	}
+
+	start() {
+
+		if (this.enabled) {
+			const error = new Error("An attempt was made to start the notifier while it is already enabled");
+			this.emit("error", error);
+		}
+		if (!this.valid) {
+			const error = new Error("An attempt was made to start the notifier but the notifier is invalid");
+			this.emit("error", error);
+		}
+		this.enabled = true;
+
+		fs.stat(this.dataFileName, (err, stat) => {
+			if (err === null) {
+				fs.readFile(this.dataFileName, "utf8", (err, json) => {
+					if (err !== null) return this.emit("error", err);
+					this.data = JSON.parse(json);
+					this.ready = true;
+				});
+
+			} else if (err.code === "ENOENT") {
+				this.data = {
+					latestVids: {}
+				}
+				this.ready = true;
+
+			} else {
+				this.emit("error", err);
+			}
+
+			setTimeout(() => {
+
+				if (!this.ready) {
+					const error = new Error("Not starting notifier as it is not ready");
+					this.emit("error", error);
+
+				} else {
+					let saveFile = false;
+					this.checkIntervalId = setInterval(() => {
+						for (let i = 0; i < this.subscriptions.length; i++) {
+
+							const channelId = this.subscriptions[i];
+
+							const req = https.request({
+								hostname: "www.youtube.com",
+								port: 443,
+								path: "/feeds/videos.xml?channel_id=" + channelId,
+								method: "GET"
+							}, (res) => {
+
+								if (res.statusCode === 404) {
+									this.unsubscribe(channelId);
+									const error = new Error("Channel \"" + channelId + "\" does not exist so has been unsubscribed from");
+									this.emit("error", error);
+									return;
+
+								} else if (res.statusCode !== 200) {
+									const error = new Error("Got status code " + res.statusCode + " when checking for new videos on channel \"" + channelId + "\"");
+									this.emit("error", error);
+									return;
+								}
+
+								let xml = "";
+								res.on("data", (chunk) => {
+									xml += chunk;
+								});
+								res.on("end", () => {
+									parseXml(xml, (err, channelData) => {
+										if (err !== null) return this.emit("error", err);
+
+										if (channelData.feed.entry.length === 0) return;
+										if (typeof this.data.latestVids[channelId] === "undefined") {
+											this.data.latestVids[channelId] = channelData.feed.entry[0]["yt:videoId"][0];
+											saveFile = true;
+											return;
+										}
+										const allVidIds = channelData.feed.entry.map(vid => vid["yt:videoId"][0]);
+										if (!allVidIds.includes(this.data.latestVids[channelId])) {
+											this.data.latestVids[channelId] = channelData.feed.entry[0]["yt:videoId"][0];
+											saveFile = true;
+											return;
+										};
+
+										let newVids = [];
+										for (let i = 0; i < channelData.feed.entry.length; i++) {
+											if (channelData.feed.entry[i]["yt:videoId"][0] === this.data.latestVids[channelId]) break;
+											const vidObj = {
+												vid: {
+													name: channelData.feed.entry[i].title[0],
+													description: channelData.feed.entry[i]["media:group"][0]["media:description"][0],
+													url: channelData.feed.entry[i].link[0].$.href,
+													id: channelData.feed.entry[i]["yt:videoId"][0],
+													width: parseInt(channelData.feed.entry[i]["media:group"][0]["media:content"][0].$.width),
+													height: parseInt(channelData.feed.entry[i]["media:group"][0]["media:content"][0].$.height),
+													thumbnail: {
+														url: channelData.feed.entry[i]["media:group"][0]["media:thumbnail"][0].$.url,
+														width: parseInt(channelData.feed.entry[i]["media:group"][0]["media:thumbnail"][0].$.width),
+														height: parseInt(channelData.feed.entry[i]["media:group"][0]["media:thumbnail"][0].$.height)
+													}
+												},
+												channel: {
+													name: channelData.feed.title[0],
+													url: channelData.feed.entry[i].author[0].uri[0],
+													id: channelData.feed["yt:channelId"][0]
+												}
+											};
+											newVids.push(vidObj);
+										};
+
+										if (newVids.length === 0) return;
+
+										saveFile = true;
+
+										newVids = newVids.reverse();
+										for (let j = 0; j < newVids.length; j++) {
+											this.emit("newVid", newVids[j]);
+										}
+										this.data.latestVids[channelId] = channelData.feed.entry[0]["yt:videoId"][0];
+									});
+								});
+							});
+
+							req.on("error", (err) => {
+								this.emit("error", err);
+							});
+
+							req.end();
+						}
+						if (saveFile) {
+							saveFile = false;
+							writeFile(this.dataFileName, JSON.stringify(this.data));
+						}
+					}, this.newVidCheckInterval);
+				}
+
+			}, 25);
+
+		});
+
+	}
+
+	stop() {
+		if (!this.enabled) {
+			const error = new Error("The stop function was ran while the notifier was disabled");
+			this.emit("error", error);
+			return;
+		}
+		this.enabled = false;
+		clearInterval(this.checkIntervalId);
+	}
+
+	subscribe(channelIds) {
+		if (typeof channelIds === "string") {
+			if (!channelIdPattern.test(channelIds)) {
+				const error = new Error("Invalid channel ID passed into subscribe function: " + channelIds);
+				return this.emit("error", error);
+			}
+			if (this.subscriptions.includes(channelIds)) {
+				const error = new Error("An attempt was made to subscribe to an already subscribed to channel: " + channelIds);
+				return this.emit("error", error);
+			}
+			this.subscriptions.push(channelIds);
+
+		} else if (Array.isArray(channelIds)) {
+			for (let i = 0; i < channelIds.length; i++) {
+				if (!channelIdPattern.test(channelIds[i])) {
+					const error = new Error("Invalid channel ID passed into subscribe function: " + channelIds[i]);
+					return this.emit("error", error);
+				}
+				if (this.subscriptions.includes(channelIds[i])) {
+					const error = new Error("An attempt was made to subscribe to an already subscribed to channel: " + channelIds[i]);
+					return this.emit("error", error);
+				}
+				this.subscriptions.push(channelIds[i]);
+			}
+
+		} else {
+			const error = new Error("Invalid datatype passed into subscribe function: " + typeof channelIds);
+			this.emit("error", error);
+		}
+	}
+
+	unsubscribe(channelIds) {
+		if (typeof channelIds === "string") {
+			if (!channelIdPattern.test(channelIds)) {
+				const error = new Error("Invalid channel ID passed into unsubscribe function: " + channelIds);
+				return this.emit("error", error);
+			}
+			if (!this.subscriptions.includes(channelIds)) {
+				const error = new Error("An attempt was made to unsubscribe from a channel which was not subscribed to: " + channelIds);
+				return this.emit("error", error);
+			}
+			this.subscriptions.splice(this.subscriptions.indexOf(channelIds), 1);
+
+		} else if (Array.isArray(channelIds)) {
+			for (let i = 0; i < channelIds.length; i++) {
+				if (!channelIdPattern.test(channelIds[i])) {
+					const error = new Error("Invalid channel ID passed into unsubscribe function: " + channelIds[i]);
+					return this.emit("error", error);
+				}
+				if (!this.subscriptions.includes(channelIds)) {
+					const error = new Error("An attempt was made to unsubscribe from a channel which was not subscribed to: " + channelIds[i]);
+					return this.emit("error", error);
+				}
+				this.subscriptions.splice(this.subscriptions.indexOf(channelIds[i]), 1);
+			}
+
+		} else {
+			const error = new Error("Invalid datatype passed into subscribe function: " + typeof channelIds);
+			this.emit("error", error);
+		}
+	}
+
+}
+
+function msg(txt, obj) {
+	if (typeof txt !== "string") {
+		const error = new Error("Invalid datatype passed into txt arg of msg function: " + typeof txt);
+		this.emit("error", error);
+		return;
+	}
+	if (!(
+		typeof obj !== "undefined" &&
+		Object.prototype.toString.call(obj) === "[object Object]" &&
+		Object.prototype.toString.call(obj.vid) === "[object Object]" &&
+		Object.prototype.toString.call(obj.vid.thumbnail) === "[object Object]" &&
+		Object.prototype.toString.call(obj.channel) === "[object Object]"
+	)) {
+		const error = new Error("Invalid video object passed into obj arg of msg function");
+		this.emit("error", error);
+		return;
+	}
+	return txt
+		.replaceAll("{vidName}", obj.vid.name)
+		.replaceAll("{vidUrl}", obj.vid.url)
+		.replaceAll("{vidDescription}", obj.vid.description)
+		.replaceAll("{vidId}", obj.vid.id)
+		.replaceAll("{vidWidth}", obj.vid.width)
+		.replaceAll("{vidHeight}", obj.vid.height)
+		.replaceAll("{vidThumbnailUrl}", obj.vid.thumbnail.url)
+		.replaceAll("{vidThumbnailWidth}", obj.vid.thumbnail.width)
+		.replaceAll("{vidThumbnailHeight}", obj.vid.thumbnail.height)
+		.replaceAll("{channelName}", obj.channel.name)
+		.replaceAll("{channelUrl}", obj.channel.url)
+		.replaceAll("{channelId}", obj.channel.id);
+}
+
+export default {
+	Notifier,
+	msg
+}
