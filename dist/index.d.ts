96a2ba0c66191c4c0b091761fff2f8f69d7c77ba
James Bennett <74157600+James-Bennett-295@users.noreply.github.com>
2024-01-20 22:44:21 +0000

    gitignore /dist/

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { PollingNotifier } from "./PollingNotifier";
-export { Video } from "./getChannelData";
-export { KeyValPairs, StorageInterface, Store } from "./storage";
-export { DebugStorage } from "./storage/DebugStorage";
-export { JsonStorage } from "./storage/JsonStorage";
-export { MemoryStorage } from "./storage/MemoryStorage";
