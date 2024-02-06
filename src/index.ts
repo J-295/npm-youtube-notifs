c260f42a102abdc6116f2454b7d92e2c4be53c1e
James Bennett <74157600+James-Bennett-295@users.noreply.github.com>
2024-02-06 18:25:06 +0000

    delete old export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ export { PollingNotifier } from "./PollingNotifier";
 export { Video } from "./getChannelData";
 export { StorageInterface, Store } from "./storage";
 
-export { DebugStorage } from "./storage/DebugStorage";
 export { JsonStorage } from "./storage/JsonStorage";
 export { MemoryStorage } from "./storage/MemoryStorage";
