af113abfcbc3ab3b9f219a8c178166e4dc48f452
James Bennett <74157600+James-Bennett-295@users.noreply.github.com>
2024-02-18 23:53:28 +0000

    storage changes

--- src/storage.ts
+++ src/storage.ts
@@ -1,71 +1,57 @@
 import fs from "node:fs";
 import path from "node:path";
 
-enum Store {
+export enum Store {
     LatestVidIds = "latest_vid_ids"
 }
 
-abstract class StorageInterface {
+export abstract class StorageInterface {
     abstract get(store: Store, keys: string[]): Promise<Record<string, string | null>>;
     abstract set(store: Store, pairs: Record<string, string>): Promise<void>;
     abstract del(store: Store, keys: string[]): Promise<void>;
 }
 
-class MemoryStorage extends StorageInterface {
+export class MemoryStorage extends StorageInterface {
     protected data: Record<string, Record<string, string>> = {};
-    async get(store: Store, keys: string[]): Promise<Record<string, string | null>> {
+    async get(store: Store, keys: string[]) {
         const pairs: Record<string, string | null> = {};
         for (const key of keys) {
             pairs[key] = this.data[store]?.[key] ?? null;
         }
         return pairs;
     }
-    async set(store: Store, pairs: Record<string, string>): Promise<void> {
+    async set(store: Store, pairs: Record<string, string>) {
         if (this.data[store] === undefined) this.data[store] = {};
         Object.assign(this.data[store]!, pairs);
     }
-    async del(store: Store, keys: string[]): Promise<void> {
+    async del(store: Store, keys: string[]) {
         for (const key of keys) {
             if (this.data[store]?.[key] !== undefined) delete this.data[store]![key];
         }
     }
 }
 
-class JsonStorage extends StorageInterface {
+export class JsonStorage extends MemoryStorage {
     private filename: string;
-    protected data: Record<string, Record<string, string>>;
     constructor(filename: string) {
         super();
         this.filename = filename;
         if (fs.existsSync(filename)) {
-            this.data = JSON.parse(fs.readFileSync(filename, { encoding: "utf-8" }));
+            this.data = JSON.parse(fs.readFileSync(filename, "utf-8"));
         } else {
             fs.mkdirSync(path.dirname(filename), { recursive: true });
             fs.writeFileSync(filename, "{}");
-            this.data = {};
         }
     }
-    async get(store: Store, keys: string[]): Promise<Record<string, string | null>> {
-        const pairs: Record<string, string | null> = {};
-        for (const key of keys) {
-            pairs[key] = this.data[store]?.[key] ?? null;
-        }
-        return pairs;
+    get(...args: Parameters<StorageInterface["get"]>) {
+        return super.get(...args);
     }
-    async set(store: Store, pairs: Record<string, string>): Promise<void> {
-        if (this.data[store] === undefined) this.data[store] = {};
-        Object.assign(this.data[store]!, pairs);
+    async set(...args: Parameters<StorageInterface["set"]>) {
+        await super.set(...args);
         fs.writeFileSync(this.filename, JSON.stringify(this.data));
     }
-    async del(store: Store, keys: string[]): Promise<void> {
-        for (const key of keys) {
-            if (this.data[store]?.[key] !== undefined) delete this.data[store]![key];
-        }
+    async del(...args: Parameters<StorageInterface["del"]>) {
+        await super.del(...args);
         fs.writeFileSync(this.filename, JSON.stringify(this.data));
     }
 }
-
-export {
-    Store, StorageInterface,
-    MemoryStorage, JsonStorage
-};
